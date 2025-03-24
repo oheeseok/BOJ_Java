ab6e11dfb2aaca1fa1be0061f20838d5eafcbf59
oheeseok <heeseok96@gmail.com>
2025-03-24 10:05:23 +0900

    구현

---
+++ src/boj2439/Main.java
@@ -0,0 +1,24 @@
+package boj2439;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int N = Integer.parseInt(input);
+
+        for (int i = 1; i <= N; i++) {
+            int cnt_space = N - i;
+            for(int j=0; j<cnt_space; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
